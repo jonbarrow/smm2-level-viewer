8e4c309458e6b9efbf1a85e5a7fae95223240db8
RedDucks <jonbarrow1998@gmail.com>
2019-08-01 19:51:09 -0400

    Update angrysun.js

--- app/assets/js/objects/enemies/angrysun.js
+++ app/assets/js/objects/enemies/angrysun.js
@@ -5,31 +5,20 @@ const Enemy = require('./Enemy');
 class AngrySun extends Enemy {
 	constructor(data) {
 		super(data);
-
-		this.spriteLoaded = false;
-		this.spriteImage = new Image();
-		this.spriteImage.addEventListener('load', () => {
-			this.spriteLoaded = true;
-		});
-	}
-
-	loadSprite() {
-		const self = this;
-
-		return new Promise(resolve => {
-			self.spriteImage.src = `./assets/img/${this.data.style}/enemies/angry_sun.png`;
-			this.spriteImage.addEventListener('load', () => {
-				this.spriteLoaded = true;
-				resolve();
-			});
-		});
+		
+		this.scene = this.data.scene;
+		this.spriteOffset = this.scene.spriteSheetData.enemies.angry_sun;
 	}
 
 	draw() {
 		this.canvasContext.drawImage(
-			this.spriteImage,
+			this.scene.spriteSheet,
+			this.spriteOffset.x,
+			this.spriteOffset.y,
+			this.spriteOffset.width,
+			this.spriteOffset.height,
 			this.data.position.x,
-			(this.data.scene.canvas.height - this.data.position.y),
+			(this.scene.canvas.height - this.data.position.y),
 			this.data.dimensions.width,
 			this.data.dimensions.height
 		);
